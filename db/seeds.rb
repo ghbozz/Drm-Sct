eb9ab1c58d999e3dfed0a004e9ac675d712199c2
romain sanson <romainsanson.gm@gmail.com>
2018-10-17 21:13:12 +0200

    pack index & navbar edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.new(email: 'toto@toto.com', password: 'mdpmdp')
+u1.save
+
+p1 = Pack.new(name: 'Kick 101', description: 'Cool kit of kicks', genre: 'Hip Hop')
+p2 = Pack.new(name: 'Hip Hop Snare 101', description: 'Lot of hip hop snares', genre: 'Hip Hop')
+p3 = Pack.new(name: 'Hi Hat 101', description: 'Ultimate Hi hat kit', genre: 'Hip Hop')
+p1.save
+p2.save
+p3.save
