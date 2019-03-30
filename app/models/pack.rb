e6534c0e436dc61674784eb5e4007e3f9a2bb190
romain sanson <romainsanson.gm@gmail.com>
2019-03-30 12:30:46 +0100

    pack validations

--- app/models/pack.rb
+++ app/models/pack.rb
@@ -1,3 +1,8 @@
 class Pack < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :genre, presence: true
+  validates :price, presence: true
+
   mount_uploader :photo, PhotoUploader
 end
